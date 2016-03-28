45829f37b0c3664e44311b375e0d38634c3b35d0
y3u8 <y3u8@petrobras.com.br>
2016-03-28 10:55:48 -0300

    versao inicial

---
+++ src/main/java/br/nom/marcio/belo/App.java
@@ -0,0 +1,13 @@
+package br.nom.marcio.belo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
